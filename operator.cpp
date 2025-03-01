c8992d81bf7313d32c2446b60a6ce46bd4ac8b87
Aditya24-rathore <ra4402538@email.com>
2025-03-01 08:24:23 -0800

    new

---
+++ operator.cpp
@@ -0,0 +1,94 @@
+
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//   int a,b;
+//   a=20;
+//   b=30;
+//   cout<<a+b;
+// }
+
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//   int a,b;
+//   a=20;
+//   b=30;
+//   cout<<a*b;
+// }
+
+// #include<iostream>
+// using namespace std;
+// int main ()
+// {
+//   int a,b,c;
+//   a=20;
+//   b=a++;
+//   cout<<b;
+// }
+
+
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//   int a,b,c;
+//   a=10;
+//   b=1;
+//   c=a&&b;
+//   cout<<c;
+//   c=a||b;
+//   cout<<c;
+//   c=!b;
+//   cout<<c;
+// }
+
+
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//   int a,b,c;
+//   a=20;
+//   b=60;
+//   c=a<b;
+//  cout<<c;
+
+// a=30;
+// b=20;
+// c=a<=b;
+// cout<<c;
+
+// c=a>b;
+// cout<<c;
+
+// c=a==b;
+// cout<<c;
+
+// c=a!=b;
+// cout<<c;
+// }
+
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     int a,b,c;
+//     a=20;
+//     b=60;
+//     c=++a;
+//     cout<<c;
+// }
+
+
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     int a,b;
+//     cout<<"enter first no";
+//     cin>>a;
+//     (a%2==0)?cout<<"even":cout<<"odd";
+// }
