023c963429cb6b1e3cfc50128945b9fe61b7b9bb
Aditya24-rathore <ra4402538@email.com>
2025-03-08 00:41:32 -0800

    update

--- laddercondition.cpp
+++ laddercondition.cpp
@@ -30,5 +30,36 @@ int main()
      else{
          cout<<c<<"is larger no";
      }
-    
 }
+
+//result
+#include<iostream>
+using namespace std;
+int main()
+{
+    float a,b,c,total,percent;
+    cout<<"enter physics no\n";
+    cin>>a;
+    cout<<"enter chemistry no\n";
+    cin>>b;
+    cout<<"enter maths no\n";
+    cin>>c;
+     total=a+b+c;
+     percent=(total/300)*100;
+    if(percent>=60 && percent<=100)
+    {
+        cout<<"result:"<<"pass with first division\n";
+    }
+    else if(percent>=50 && percent<=59)
+    {
+         cout<<"pass with second  division\n";
+    }
+    else if(percent>=35 && percent<=49)
+    {
+         cout<<"pass with third  division\n";
+    }
+    else{
+        cout<<"result:"<<"fail";
+    }
+}
+
